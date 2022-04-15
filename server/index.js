75757e74f072f43a3d8da156c2208a1272031140
truong.nat <truongdq.dev@gmail.com>
2022-04-15 20:57:33 +0700

    resolve all router on server

--- server/index.js
+++ server/index.js
@@ -1,31 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
-const { connectMongoose } = require("./connection");
-const authRouter = require("./router/user.router");
-const todosRouter = require("./router/todos.router");
-const path = require("path");
-require("dotenv").config();
+const { connectMongoose } = require('./connection');
+const authRouter = require('./router/user.router');
+const todosRouter = require('./router/todos.router');
+const path = require('path');
+require('dotenv').config();
 connectMongoose();
 app.use(
   cors({
-    origin: "*",
+    origin: '*',
   })
 );
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "build")));
-app.use("/auth", authRouter);
-app.use("/todos", todosRouter);
-app.get("/ssr", (req, res) => {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use('/auth', authRouter);
+app.use('/todos', todosRouter);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
-app.get("/", (req, res) => {
-  res.send("it work!");
-});
 app.listen(process.env.PORT, () => {
-  console.log("server on started");
+  console.log('server on started');
 });
--- server/router/user.router.js
+++ server/router/user.router.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const {userController} = require("../controllers/user.controller");
-const {authMiddleware} = require("../middleware");
+const { userController } = require('../controllers/user.controller');
+const { authMiddleware } = require('../middleware');
 
-router.post("/register", userController.create);
-router.post("/login", userController.login);
-router.get("/checking-me", authMiddleware, userController.checkingMe);
-router.post('/update', authMiddleware, userController.update)
-router.get("/logout", userController.logout);
+router.post('/register', userController.create);
+router.post('/login', userController.login);
+router.get('/checking-me', authMiddleware, userController.checkingMe);
+router.post('/update', authMiddleware, userController.update);
+router.get('/logout', userController.logout);
 
 module.exports = router;
