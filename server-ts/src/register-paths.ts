ddc033cec6af1de28cc14d991072d76a635d7e10
Peanut <truongdq.dev@gmail.com>
2026-03-17 13:39:39 +0700

    chore: clean up folder structure

--- server-ts/src/register-paths.ts
+++
@@ -1,7 +0,0 @@
-import { register } from 'tsconfig-paths';
-import { compilerOptions } from '../tsconfig.json';
-
-register({
-  baseUrl: compilerOptions.baseUrl,
-  paths: compilerOptions.paths,
-});
