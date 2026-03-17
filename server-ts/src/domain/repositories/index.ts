d18c255e7656c58c21c07c0cd167dd856c6d6a09
Peanut <truongdq.dev@gmail.com>
2026-03-17 13:38:45 +0700

    chore: clean up folder structure

--- server-ts/src/domain/repositories/index.ts
+++
@@ -1,4 +0,0 @@
-export { IUserRepository } from './IUserRepository';
-export { ITodoRepository } from './ITodoRepository';
-export { IRoomRepository } from './IRoomRepository';
-export { IMessageRepository } from './IMessageRepository';
